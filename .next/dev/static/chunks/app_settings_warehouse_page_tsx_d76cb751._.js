ab07db2f466b3b2cf411ab292b4dcff84f47c51c
Dhaval Rathva <dmr19104@gmail.com>
2025-11-22 16:33:37 +0530

    Remove node_modules and .next from repo

--- .next/dev/static/chunks/app_settings_warehouse_page_tsx_d76cb751._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_3b0c0cf7._.js"
-],
-    source: "dynamic"
-});
--- .next/server/app/api/auth/[...nextauth]/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/[...nextauth]/route.js")
-R.c("server/chunks/[root-of-the-server]__7c2d97f1._.js")
-R.c("server/chunks/node_modules_next_7f922d58._.js")
-R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
-R.c("server/chunks/_next-internal_server_app_api_auth_[___nextauth]_route_actions_1c865db8.js")
-R.m(59061)
-module.exports=R.m(59061).exports
